b47602711482bb0ff25b7b1cd0b5d2599172a52d
Tim Rye <tim@tenfold.media>
2016-02-17 15:38:57 +0000

    Fixed login button styling
    text-shadow needed overriding since WP 4.4

--- tenfold-white-label.php
+++ tenfold-white-label.php
@@ -5,7 +5,7 @@ Plugin URI:        http://tenfold.media
 Description:       This plugin customises WordPress for Tenfold Media clients, adding features, cleaning up unneccesary things and generally improving WordPress. Make sure the plugin 'GitHub Updater' is activated to recieve updates to this plugin.
 Author:            Tim Rye
 Author URI:        http://tenfold.media/tim
-Version:           1.0.0
+Version:           1.0.1
 GitHub Plugin URI: TenfoldMedia/tenfold-white-label
 GitHub Branch:     master
 ******************************************************************/
@@ -26,26 +26,22 @@ CUSTOMISE LOGIN AREA
 *********************/
 
 // calling your own login css so you can style it
-function tf_custom_login_logo() {
-    echo '<style type="text/css">';
-	echo '.login h1 a {
-		background: url(' . plugins_url('login-logo.png', __FILE__) . ') no-repeat top center;
-		width: 320px;
-		height: 146px;
-		text-indent: -9999px;
-		overflow: hidden;
-		padding-bottom: 15px;
-		display: block;
-	}';
-	echo '#wp-submit {
-		background: #d72020;
-		border-color: #be0707;
-		-webkit-box-shadow: none;
-		-moz-box-shadow: none;
-		box-shadow: none;
-	}';
-	echo '</style>';
-}
+function tf_custom_login_logo() { ?>
+    <style type="text/css">
+		.login h1 a {
+			width: 320px;
+			height: 146px;
+			background: url(<?php echo plugins_url('login-logo.png', __FILE__) ?>) no-repeat top center;
+		}
+		#wp-submit {
+			background: #d72020;
+			border-color: #be0707;
+			-webkit-box-shadow: 0 1px 0 #8e0404;
+			box-shadow: 0 1px 0 #8e0404;
+			text-shadow: 0 -1px 1px #8e0404,1px 0 1px #8e0404,0 1px 1px #8e0404,-1px 0 1px #8e0404;
+		}
+	</style>
+<?php }
 add_action('login_head', 'tf_custom_login_logo');
 
 // changing the logo link from wordpress.org to your site
@@ -58,8 +54,7 @@ add_filter('login_headertitle', 'tf_login_title');
 
 // set 'remember me' to checked
 function tf_login_check_remember_me_script() { echo "<script>document.getElementById('rememberme').checked = true;</script>"; }
-function tf_login_check_remember_me_setup() { add_filter('login_footer', 'tf_login_check_remember_me_script'); }
-add_action('init', 'tf_login_check_remember_me_setup');
+add_filter('login_footer', 'tf_login_check_remember_me_script');
 
 
 /*********************
@@ -135,11 +130,11 @@ function tf_cleanup() {
 	remove_action('wp_head', 'index_rel_link');							// index link
 	remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);	// links for adjacent posts
 	remove_action('wp_head', 'wp_generator');							// WP version
-	
+
 	add_filter('the_generator', 'tf_remove_wp_ver_rss');				// remove WP version from RSS
 	add_filter('style_loader_src', 'tf_remove_wp_ver_css_js', 9999);	// remove WP version from css
 	add_filter('script_loader_src', 'tf_remove_wp_ver_css_js', 9999);	// remove WP version from scripts
-	
+
 	add_filter('the_content', 'tf_filter_ptags_on_images');				// cleaning up random code around images
 }
 add_action('after_setup_theme', 'tf_cleanup', 11);
@@ -148,7 +143,7 @@ add_action('after_setup_theme', 'tf_cleanup', 11);
 function tf_disable_dashboard_widgets() {
 	remove_meta_box('dashboard_quick_press', 'dashboard', 'core');			// Quick Draft widget
 	remove_meta_box('dashboard_primary', 'dashboard', 'core');				// WordPress News widget
-    
+
 	update_user_meta(get_current_user_id(), 'show_welcome_panel', false);	// Remove the welcome panel
 }
 add_action('admin_menu', 'tf_disable_dashboard_widgets');
